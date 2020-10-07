6b7e82476ea26a5d054e9d09ff6c1b97fbcfe7db
Troll Coder <math323n@edu.campusvejle.dk>
2020-10-07 13:27:26 +0200

    Adddded someeethingg,?.

--- S4.SocialMedia.Entities/Models/Context/SocialMediaContext.cs
+++ S4.SocialMedia.Entities/Models/Context/SocialMediaContext.cs
@@ -30,7 +30,6 @@ namespace S4.SocialMedia.Entities.Models.Context
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
                 optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SocialMediaDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
