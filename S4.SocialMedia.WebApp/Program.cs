a9d9614cb7ab6644c5f033969ae46c56ce39833b
Troll Coder <math323n@edu.campusvejle.dk>
2020-11-04 09:27:02 +0100

    stuff

--- S4.SocialMedia.WebApp/Program.cs
+++ S4.SocialMedia.WebApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 
 namespace S4.SocialMedia.WebApp
 {
@@ -12,6 +14,13 @@ namespace S4.SocialMedia.WebApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(
+keyVaultEndpoint,
+new DefaultAzureCredential());
+})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
